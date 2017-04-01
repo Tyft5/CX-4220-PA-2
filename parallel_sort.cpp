fb5ef51df90ab105cf166a6accb7b3d63a0c6c96
Tyft5 <ttippens6@gatech.edu>
2017-04-01 16:23:12 -0400

    Comments for code segments

--- parallel_sort.cpp
+++ parallel_sort.cpp
@@ -10,6 +10,36 @@
 
 // implementation of your parallel sorting
 void parallel_sort(int * begin, int* end, MPI_Comm comm) {
+
+    // Terminating condition
+
+
+    // Call seeding helper function
+
+
+    // Generate a pivot
+    // Check for pivot in local array
+    // If you have the pivot, broadcast it, otherwise receive it
+
+
+    // Split local array based on pivot
+    //  - allocate second array
+    //  - keep track of size of both arrays
+
+
+    // Allgather to find total # of elements < and > pivot
+
+
+    // Decide # of processors for < and > pivot
+
+
+    // Send < and > arrays to appropriate processors (using alltoall)
+
+
+    // Create two new communicators
+
+
+
 }
 
 
@@ -18,3 +48,5 @@ void parallel_sort(int * begin, int* end, MPI_Comm comm) {
  *********************************************************************/
 
 // ...
+
+// Function to seed RNG once with the same seed on each processor
